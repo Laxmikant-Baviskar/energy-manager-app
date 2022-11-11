eefb9b7551b12df7b3018dfbd33899aacb406c5a
vedant <ursvedantyetekar@gmail.com>
2022-11-12 01:27:59 +0530

    cleaned up unnecessary files and created ui for register page

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useLayoutEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WebFont from "webfontloader";
+import "react-toastify/dist/ReactToastify.css";
+import Register from "./components/register/register";
+import Login from "./components/login/login";
+import Manager from "./components/manager/manager";
+
+const App = () => {
+  useLayoutEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Dosis", "Nunito", "Montez"],
+      },
+    });
+  });
+
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Manager />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
